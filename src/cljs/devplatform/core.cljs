96490157086124d090242a16953f6119a41812bb
Moritz Grauel <mo@notadomain.com>
2014-08-17 11:34:08 +0200

    plan extraction

--- src/cljs/devplatform/core.cljs
+++ src/cljs/devplatform/core.cljs
@@ -1,8 +1,31 @@
-(ns devplatform.core)
+(ns devplatform.core
+  (:require [clojure.string :refer [split trim blank? replace]]))
 
 (enable-console-print!)
 
-(println "Hallo, Cljs!")
+(def doc window/document)
+(def planInput (.getElementById doc "planInput"))
 
+(defn pipelog [x] (.log window/console x) x)
 
-(defn add [a b] (+ a b))
+(defn extractPlan [inputString]
+  (-> inputString
+      (split "plan")
+      (->> (map trim))
+      (->> (remove blank?))
+      (->> (map #(replace % #"[\(\)]" "")))
+      (->> (map #(split % ",")))
+      (->> (map #(zipmap [:day :shift :name] %)))))
+
+(defn updatePlan [e]
+  (let [t (.-target e)
+        v (.-value t)
+        p (extractPlan v)]
+    (println p)))
+
+(defn setup! []
+  (.addEventListener planInput "change" updatePlan))
+
+#_(setup!)
+
+(extractPlan (.-value planInput))
