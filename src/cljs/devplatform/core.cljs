dfbc63e504577059238d28d1be99d0d436758b49
Moritz Grauel <mo@notadomain.com>
2014-08-17 13:12:14 +0200

    planning

--- src/cljs/devplatform/core.cljs
+++ src/cljs/devplatform/core.cljs
@@ -1,5 +1,5 @@
 (ns devplatform.core
-  (:require [clojure.string :refer [split trim blank? replace]]))
+  (:require [clojure.string :refer [split trim blank? replace join lower-case]]))
 
 (enable-console-print!)
 (def doc window/document)
@@ -7,27 +7,54 @@
 (def renderInput (.getElementById doc "render"))
 (def d3 window/d3)
 
-(def shift-color ["red" "green" "blue" "yellow"])
+(def WIDTH 60)
+(def HEIGHT-HOUR 20)
+(def HEIGHT (* 8 HEIGHT-HOUR))
 
-(def WIDTH 40)
-(def HEIGHT 160)
+(defn shift-color [name]
+  (let [toNumber #(-> (window/parseInt (nth (lower-case %1) %2) 36)
+                     (/ 36)
+                     (* 255)
+                     (int))]
+    (str "rgb(" (join "," [(toNumber name 0) (toNumber name 1) (toNumber name 2)]) ")")))
+
+(println (shift-color "Wally"))
+(println (shift-color "bob"))
 
 (defn plotPlan [plan]
   (let [jsPlan (clj->js plan)
         dataSet (-> d3
                     (.select "#canvas")
-                    (.selectAll "rect")
-                    (.data jsPlan))
+                    (.selectAll "g")
+                    (.data jsPlan (fn [key] (println key) (str
+                                                            (.-day key)
+                                                            (.-shift key)
+                                                            (.-name key)))))
         enterSet (.enter dataSet)
+        enterGroup (-> enterSet
+                     (.append "g")
+                     (.attr "transform" #(str "translate(" (* WIDTH (.-day %)) ",0)")))
         exitSet (.exit dataSet)]
-    (-> enterSet
+    (-> enterGroup
         (.append "rect")
-        (.attr #js {:width WIDTH :height 1})
-        (.style "fill" (fn [d] (nth shift-color (.-shift d) 0)))
+        (.attr #js {:width WIDTH :height 1 :class "block"})
+        (.style "fill" #(shift-color (.-name %)))
+        (.style "stroke" 1))
+    (-> enterGroup
+        (.append "text")
+        (.style "fill" "white")
+        (.attr #js {:y (/ HEIGHT 2) :x 3})
         (.text #(.-name %)))
     (-> dataSet
         (.transition)
+        (.duration 1000)
+        (.attr "transform" #(str "translate("
+                                 (* WIDTH (.-day %))
+                                 ","
+                                 (* HEIGHT-HOUR (dec (.-shift %))) ")"))
+        (.selectAll ".block")
         (.attr #js {:height HEIGHT}))
+
     (-> exitSet
         (.remove))))
 
@@ -48,6 +75,6 @@
 (defn setup! []
   (.addEventListener renderInput "click" updatePlan))
 
-#_(setup!)
+(setup!)
 
-(plotPlan [{:day 1 :shift 1 :name Bob}] )
+#_(plotPlan [{:day 1 :shift 1 :name "Bob"} {:day 1 :shift 2 :name "Betty"}] )
