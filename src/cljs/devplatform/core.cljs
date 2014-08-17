867e28ba2d770c4ae5364ec13f965c29d9107542
Moritz Grauel <mo@notadomain.com>
2014-08-17 12:21:14 +0200

    some painting

--- src/cljs/devplatform/core.cljs
+++ src/cljs/devplatform/core.cljs
@@ -2,30 +2,52 @@
   (:require [clojure.string :refer [split trim blank? replace]]))
 
 (enable-console-print!)
-
 (def doc window/document)
 (def planInput (.getElementById doc "planInput"))
+(def renderInput (.getElementById doc "render"))
+(def d3 window/d3)
 
-(defn pipelog [x] (.log window/console x) x)
+(def shift-color ["red" "green" "blue" "yellow"])
+
+(def WIDTH 40)
+(def HEIGHT 160)
+
+(defn plotPlan [plan]
+  (let [jsPlan (clj->js plan)
+        dataSet (-> d3
+                    (.select "#canvas")
+                    (.selectAll "rect")
+                    (.data jsPlan))
+        enterSet (.enter dataSet)
+        exitSet (.exit dataSet)]
+    (-> enterSet
+        (.append "rect")
+        (.attr #js {:width WIDTH :height 1})
+        (.style "fill" (fn [d] (nth shift-color (.-shift d) 0)))
+        (.text #(.-name %)))
+    (-> dataSet
+        (.transition)
+        (.attr #js {:height HEIGHT}))
+    (-> exitSet
+        (.remove))))
 
 (defn extractPlan [inputString]
-  (-> inputString
-      (split "plan")
-      (->> (map trim))
-      (->> (remove blank?))
-      (->> (map #(replace % #"[\(\)]" "")))
-      (->> (map #(split % ",")))
-      (->> (map #(zipmap [:day :shift :name] %)))))
+  (->> (split inputString "plan")
+       (map trim)
+       (remove blank?)
+       (map #(replace % #"[\(\)]" ""))
+       (map #(split % ","))
+       (map #(zipmap [:day :shift :name] %))))
 
 (defn updatePlan [e]
-  (let [t (.-target e)
-        v (.-value t)
+  (println e)
+  (let [v (.-value planInput)
         p (extractPlan v)]
-    (println p)))
+    (plotPlan p)))
 
 (defn setup! []
-  (.addEventListener planInput "change" updatePlan))
+  (.addEventListener renderInput "click" updatePlan))
 
 #_(setup!)
 
-(extractPlan (.-value planInput))
+(plotPlan [{:day 1 :shift 1 :name Bob}] )
